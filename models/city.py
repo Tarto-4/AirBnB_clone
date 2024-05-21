d85126b129a086b92ef000765056032f8176ce84
Thato Mongwe <thatomongwe47@gmail.com>
2024-05-21 21:00:39 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -7,16 +7,23 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """class City that inherits from BaseModel
-    public class attributes:
-    state_id: string - empty string
-    name: string - empty string
+    """
+    Class City that inherits from BaseModel
+
+    Attributes:
+        state_id: string - ID of the State the city belongs to (public class attribute)
+        name: string - Name of the city (public class attribute)
     """
 
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """create new city
+        """
+        Creates a new City instance
+
+        Args:
+            *args: Arguments passed to the parent class constructor
+            **kwargs: Keyword arguments passed to the parent class constructor
         """
         super().__init__(self, *args, **kwargs)
