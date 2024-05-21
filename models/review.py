38e61284f95666580faf1030cbf37c56645af27f
Thato Mongwe <thatomongwe47@gmail.com>
2024-05-21 21:24:26 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -8,25 +8,11 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """
-    Class Review that inherits from BaseModel
-
-    Attributes:
-        place_id (str): ID of the Place the review belongs to
-        user_id (str): ID of the User who wrote the review
-        text (str): Text content of the review
+    Class representing a Review
     """
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    place_id = str
+    user_id = str
+    text = str
 
     def __init__(self, *args, **kwargs):
-        """
-        Creates a new Review instance
-
-        Args:
-            *args: Arguments passed to the parent class constructor
-            **kwargs: Keyword arguments passed to the parent class constructor
-        """
-        super().__init__(self, *args, **kwargs)
-
