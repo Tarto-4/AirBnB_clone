d3cadf975eca029931150a450f2e6a624167fd0d
Thato Mongwe <thatomongwe47@gmail.com>
2024-05-21 21:23:52 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -8,40 +8,27 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """
-    Class Place that inherits from BaseModel
-
-    Attributes:
-        city_id: str - ID of the City the place belongs to (public class attribute)
-        user_id: str - ID of the User who owns the place (public class attribute)
-        name: str - Name of the place (public class attribute)
-        description: str - Description of the place (public class attribute)
-        number_rooms: int - Number of rooms in the place (public class attribute)
-        number_bathrooms: int - Number of bathrooms in the place (public class attribute)
-        max_guest: int - Maximum number of guests allowed in the place (public class attribute)
-        price_by_night: int - Price per night for the place (public class attribute)
-        latitude: float - Latitude coordinate of the place (public class attribute)
-        longitude: float - Longitude coordinate of the place (public class attribute)
-        amenity_ids: list[str] - List of Amenity.id strings associated with the place (public class attribute)
+    Class representing a Place
     """
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids: list[str] = []
+    city_id = str
+    user_id = str
+    name = str
+    description = str
+    number_rooms = int
+    number_bathrooms = int
+    max_guest = int
+    price_by_night = int
+    latitude = float
+    longitude = float
+    amenity_ids = list
 
     def __init__(self, *args, **kwargs):
         """
-        Creates a new Place instance
+        Initializes the Place object
 
         Args:
-            *args: Arguments passed to the parent class constructor
-            **kwargs: Keyword arguments passed to the parent class constructor
+            *args (optional): Arguments passed to the parent class constructor
+            **kwargs (dict): Keyword arguments for Place attributes
         """
         super().__init__(self, *args, **kwargs)
